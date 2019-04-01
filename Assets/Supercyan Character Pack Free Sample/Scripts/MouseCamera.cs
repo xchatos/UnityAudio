eaa3e70bb7327ce1367890daa7331a8a61a851d8
Juuso <juusoetela@gmail.com>
2019-04-01 15:27:10 +0300

    no message

---
+++ Assets/Supercyan Character Pack Free Sample/Scripts/MouseCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamera : MonoBehaviour
+{
+    public Transform target;
+
+    float xAxis;
+    float yAxis;
+
+    public float distance = 10.0f;
+    public float height;
+    public float speed = 5.0f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void LateUpdate()
+    {
+
+        xAxis += Input.GetAxis("Mouse X") * speed;
+        yAxis  -= Input.GetAxis("Mouse Y") * speed;
+
+        Quaternion rotation = Quaternion.Euler(yAxis, xAxis, 0.0f);
+
+        transform.rotation = rotation;
+
+        transform.position = target.position + rotation * new Vector3(0.0f, height, -distance);
+    }
+
+
+}
