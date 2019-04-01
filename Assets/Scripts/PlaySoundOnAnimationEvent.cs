91e1f6932b6e5e11fb2e5a5eedb81a486b905d97
Jaakko Liukkala <jaakko.liukkala@cs.tamk.fi>
2019-04-01 18:23:03 +0300

    Created most of the resources needed for the Animation Events section of the lesson.

---
+++ Assets/Scripts/PlaySoundOnAnimationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlaySoundOnAnimationEvent : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+
+    private AudioSource audioSource;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        int random = Random.Range(0, audioClips.Length);
+
+        audioSource.PlayOneShot(audioClips[random]);
+    }
+}
